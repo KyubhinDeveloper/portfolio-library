e36458f8c284b4a45c642ceb68b48e17066a9326
한규빈 <hanguybeen@naver.com>
2022-08-05 08:58:13 +0900

    refactor: sideMenu 클릭 이벤트 수정
    overflow hidden 효과가 먼저 적용되면서 사이드 메뉴가 보여지는 속도에 딜레이가 생겨 setTime out을 이용해
    overflow hidden 효과가 나중에 적용되게 해서 딜레이가 없도록 함.

--- portfolio-library/src/main/resources/static/js/main.js
+++ portfolio-library/src/main/resources/static/js/main.js
@@ -84,14 +84,16 @@ $('.move-top-icon').click(function(){
 
 //사이드 메뉴 이벤트
 $('.hamberger-box').click(function(){
-    $('body').css('overflow', 'hidden');
     $('.side-menu-bg').addClass('menu-show');
-
+    setTimeout(()=>{
+		$('body').css('overflow', 'hidden')
+	},300);
 })
 
 $(document).on('click','.btn-close-side', function(){	
-    $('body').css('overflow', 'auto');
+	  
     $('.side-menu-bg').removeClass('menu-show');
+    $('body').css('overflow', 'auto');
 })
 
 //사이드 메뉴 탭 클릭 이벤트
